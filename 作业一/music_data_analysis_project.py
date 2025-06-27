20fa035b53c502f303069f3658cdfc9d3d7cf79b
13119 <1311940289@qq.com>
2025-06-27 16:57:25 +0800

    Merge data-exploration into main

--- 作业一/music_data_analysis_project.py
+++ 作业一/music_data_analysis_project.py
@@ -4,8 +4,10 @@ import pandas as pd
 df = pd.read_csv("nigerian-songs.csv")#以pandas库的read_csv函数读取csv文件
 print(df.head(5))#查看前5行数据
 
-df.info()
+print(df.info())
 
-df.describe()
+print(df.describe())
+
+
 
 

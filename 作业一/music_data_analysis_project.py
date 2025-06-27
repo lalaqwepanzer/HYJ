90e30ae137055de0d87b1aec1e4549fe9770a45a
13119 <1311940289@qq.com>
2025-06-27 16:28:50 +0800

    delete

--- 作业一/music_data_analysis_project.py
+++ 作业一/music_data_analysis_project.py
@@ -2,9 +2,5 @@ import matplotlib.pyplot as plt
 import pandas as pd
  
 df = pd.read_csv("nigerian-songs.csv")#以pandas库的read_csv函数读取csv文件
-df.head()#查看前5行数据
-
-df.info()
-
-df.describe()
+print(df.head(5))#查看前5行数据
 

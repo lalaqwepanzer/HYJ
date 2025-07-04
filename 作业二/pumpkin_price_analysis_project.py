9a12b4ce5f5204feaabdcfa0db1dc057ae4620ef
13119 <1311940289@qq.com>
2025-07-04 14:48:11 +0800

    数据探索

--- 作业二/pumpkin_price_analysis_project.py
+++ 作业二/pumpkin_price_analysis_project.py
@@ -1,3 +1,10 @@
 import pandas as pd
 pumpkins = pd.read_csv('US-pumpkins.csv')
 print(pumpkins.head())
+
+print(pumpkins.head())#查看数据的前5行
+print(pumpkins.tail())#查看数据的后5行
+print(pumpkins.info()) #查看数据的组织结构
+
+print(pumpkins.isnull().sum()) #检查每列数据中的缺失值的数量
+print(pumpkins["Package"].is_unique)
